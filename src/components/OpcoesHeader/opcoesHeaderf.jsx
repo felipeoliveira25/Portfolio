4b7284d472914a122768efd73549460115780982
Felipe Oliveira Ferreira <119983478+felipeoliveira25@users.noreply.github.com>
2024-03-12 19:39:07 -0300

    Update opcoesHeaderf.jsx

--- src/components/OpcoesHeader/opcoesHeaderf.jsx
+++ src/components/OpcoesHeader/opcoesHeaderf.jsx
@@ -9,7 +9,7 @@ function OpcoesHeader(){
             <Opcao>Home</Opcao>
             <Opcao>Projetos</Opcao>
             <Opcao>Skills</Opcao>
-            <Botao cor="#31363F">Contato</Botao>
+            <Botao cor="rgba(0, 245, 160, 1)">Contato</Botao>
         </Lista>
     )
 }
