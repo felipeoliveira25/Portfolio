3683f8bbe864de32f71f4b52c0b0188bbdcb0895
Felipe Oliveira Ferreira <119983478+felipeoliveira25@users.noreply.github.com>
2024-03-12 19:42:06 -0300

    Update descricaoinicial.jsx

--- src/components/DescricaoInicial/descricaoinicial.jsx
+++ src/components/DescricaoInicial/descricaoinicial.jsx
@@ -11,7 +11,7 @@ function DescricaoInicial(){
                 <Descricao>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo nisi suscipit assumenda voluptatem. Reprehenderit ipsum enim tempore mollitia iusto rem a consequatur quam repellat voluptas! Dolorem ullam harum nam quod?
                 </Descricao>
-                <Botao cor="#31363F">Entre em Contato</Botao>
+                <Botao cor="#31363F" backgroundColor="rgba(0, 245, 160, 1)">Entre em Contato</Botao>
             </DivTextos>
                 
            
